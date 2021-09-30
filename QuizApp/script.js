b1f5f1677c061f3804d20a44991c7114a4379645
itscsp <cspoojary7972@gmail.com>
2021-09-30 08:45:38 +0530

    Design Quiz App

---
+++ QuizApp/script.js
@@ -0,0 +1,30 @@
+const quizData = [
+    {
+      "id": 1,
+      "question": "How to delete a directory in Linux?",
+        a: '10',
+        b: '17',
+        c: '26',
+        d: '110',
+        correct: 'c'
+    },
+    {
+        "id": 2,
+        "question": "Favirate Programing language",
+          a: 'Java',
+          b: 'JavaScrip',
+          c: 'Python',
+          d: 'C++',
+          correct: 'b'
+    },
+    {
+        "id": 3
+        ,
+        "question": "What do Love Do",
+          a: 'Play',
+          b: 'travling',
+          c: 'Coding',
+          d: 'SEX',
+          correct: 'C'
+    }
+  ]
