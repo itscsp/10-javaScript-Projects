32bab702f68616484b5620d7ea52990b2d19fc2e
itscsp <cspoojary7972@gmail.com>
2021-09-30 07:15:59 +0530

    New Year CountDown timer

---
+++ countdown-timer/script.js
@@ -0,0 +1,44 @@
+// To Get Dom Elements
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+
+
+function countdown() {
+    const newYears = "1 Jan 2022";
+
+
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    // To Get Total Seconds in remaining
+    const totalSeconds = (newYearsDate - currentDate) / 1000 ;
+
+    // To Get days
+    const days = Math.floor(totalSeconds / 3600 / 24);
+
+    // To Get Hours
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    // To Get Minutes
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    // To Get seconds
+    const seconds = Math.floor(totalSeconds % 60)
+
+    daysEl.innerHTML = formatedTime(days);
+    hoursEl.innerHTML = formatedTime(hours);
+    minsEl.innerHTML = formatedTime(minutes);
+    secondsEl.innerHTML = formatedTime(seconds);
+
+}
+
+//Function to return Formatef Time
+function formatedTime(time){
+    return time < 10 ? (`0${time}`) : time;
+}
+
+// first call of the function
+countdown();
+
+setInterval(countdown, 1000)
